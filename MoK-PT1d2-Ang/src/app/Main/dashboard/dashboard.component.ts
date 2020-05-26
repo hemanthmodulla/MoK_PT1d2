b0fec9bd3b3f887520fae1f5baa93064fb695ba3
heman <hemanthkumar.m13@gmail.com>
2020-05-26 17:05:19 -0400

    added dashboard and side nav

--- MoK-PT1d2-Ang/src/app/Main/dashboard/dashboard.component.ts
+++ MoK-PT1d2-Ang/src/app/Main/dashboard/dashboard.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styles: [
-  ]
+  styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-
+  activewrapper = true;
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  ShowSideNav(event: Event) {
+    this.activewrapper = !this.activewrapper;
+  }
 }
--- MoK-PT1d2-Ang/src/app/app.module.ts
+++ MoK-PT1d2-Ang/src/app/app.module.ts
@@ -9,9 +9,17 @@ import { LoginService } from './services/login.service';
 import {FormsModule} from '@angular/forms';
 import { ToastrModule } from 'ngx-toastr';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatToolbarModule, } from '@angular/material/toolbar';
+import { MatIconModule, } from '@angular/material/icon';
+import { MatSidenavModule, } from '@angular/material/sidenav';
+import { MatListModule, } from '@angular/material/list';
+import { MatButtonModule, } from '@angular/material/button';
 import {BsDatepickerModule} from 'ngx-bootstrap/datepicker';
 import { ProfileinfoComponent } from './personal/profileinfo/profileinfo.component';
 import { DashboardComponent } from './Main/dashboard/dashboard.component';
+import { SidenavComponent } from './Main/sidenav/sidenav.component';
+import { DashboardBodyComponent } from './Main/dashboard-body/dashboard-body.component';
+
 
 
 @NgModule({
@@ -20,7 +28,9 @@ import { DashboardComponent } from './Main/dashboard/dashboard.component';
     LoginComponent,
     RegistrationComponent,
     ProfileinfoComponent,
-    DashboardComponent
+    DashboardComponent,
+    SidenavComponent,
+    DashboardBodyComponent,
   ],
   imports: [
     BrowserModule,
@@ -29,7 +39,12 @@ import { DashboardComponent } from './Main/dashboard/dashboard.component';
     FormsModule,
     BrowserAnimationsModule,
     BsDatepickerModule.forRoot(),
-    ToastrModule.forRoot()
+    ToastrModule.forRoot(),
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,
   ],
   providers: [LoginService],
   bootstrap: [AppComponent]
